ae0c96848a714b7f281bba20234a2fbb5821dcfb
Elnur <elnurveliyev13@yahoo.com>
2025-11-03 00:33:47 +0100

    trying to increace a font of the banner

--- lang/en/flashcards.php
+++ lang/en/flashcards.php
@@ -70,9 +70,11 @@ $string['fill_field'] = 'Please fill: {$a}';
 $string['autofill_soon'] = 'Auto-fill will be available soon';
 
 // iOS Install Instructions
-$string['ios_install_title'] = 'Install this app';
-$string['ios_install_step1'] = 'Tap the';
-$string['ios_install_step2'] = 'button, then select';
+$string['ios_install_title'] = 'Install this app on your Home Screen:';
+$string['ios_install_step1'] = '1. Tap the';
+$string['ios_install_step1_suffix'] = 'button.';
+$string['ios_install_step2'] = '2. Select';
+$string['ios_install_step2_suffix'] = '';
 $string['ios_share_button'] = 'Share';
 $string['ios_add_to_home'] = 'Add to Home Screen';
 
