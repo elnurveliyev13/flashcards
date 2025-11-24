d863aa17fac1adb0ab8aa9425e52a27260c03121
Elnur <elnurveliyev13@yahoo.com>
2025-11-25 00:07:19 +0100

    fixing height of dictation-move-block

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "srs-cache-v7";
+const CACHE = "srs-cache-v8";
 const ASSETS = [
   // Empty cache - Service Worker will be registered but won't cache anything
   // This is minimal config just to enable PWA install prompt
