2f045bbe0fe097f2511af26eb3e69a56508e7e5b
Elnur <elnurveliyev13@yahoo.com>
2025-11-24 23:24:56 +0100

    увеличиваем номер версии кэша, чтобы изменения заработали

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "srs-cache-v3";
+const CACHE = "srs-cache-v4";
 const ASSETS = [
   // Empty cache - Service Worker will be registered but won't cache anything
   // This is minimal config just to enable PWA install prompt
