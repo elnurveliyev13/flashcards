641bcbb5a038298350b0f70ffdfc0fdabb8e5cec
Elnur <elnurveliyev13@yahoo.com>
2025-11-25 00:36:05 +0100

    добавляем вычекивание лишних слов

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "srs-cache-v10";
+const CACHE = "srs-cache-v11";
 const ASSETS = [
   // Empty cache - Service Worker will be registered but won't cache anything
   // This is minimal config just to enable PWA install prompt
